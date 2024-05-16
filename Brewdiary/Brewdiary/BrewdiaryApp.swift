1eba3cf557e8e332724d3ed85736434ce2aa3b13
Davies Luo <daviesluo@gmail.com>
2024-05-16 08:20:23 +0100

    Initial commit

---
+++ Brewdiary/Brewdiary/BrewdiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewdiaryApp.swift
+//  Brewdiary
+//
+//  Created by Davies Luo on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BrewdiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
